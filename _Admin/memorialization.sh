ca39946e407e1efab73b23300d644144043e60fd
Steven Jenkins <45439182+SJ-Hydra@users.noreply.github.com>
2020-04-01 13:30:11 -0400

    Update memorialization.sh

--- _Admin/memorialization.sh
+++ _Admin/memorialization.sh
@@ -1,18 +1,59 @@
 #!/bin/bash
-## Memorialization Version 1.0
+## Memorialization Tool Version 0.1
+## Author: SJ-Hydra
 
-echo "This tool is used to compile information needed for engagement memorialization. If your client asks you for scans, then you'll have them."
-echo "Use: ./memorialization.sh <file containing list of IPs, one IP or range, per line."
+# Colors
+GREY='\033[0;34m'
+RED='\033[0;31m' 
+NC='\033[0m' #No Color
 
-sed -n '2p' ./script.sh
-read $ip
 
-nmap -Pn $ip > $ip_commontcp_nmap.txt;
+# Introduction
+sed -n '2p' ./memorialization.sh
+sed -n '3p' ./memorialization.sh
+
+echo -e "${GREY}This tool is used to compile information needed for engagement memorialization.${NC}"
+echo -e "${GREY}If your client asks you for scans, then you'll have them.\n${NC}"
+echo -e "${GREY}Use: ./memorialization.sh <IPs>${NC}\n"
+
+
+# Sudo Checker, sudo permission needed for following scans to complete. 
+if ! [ $(id -u) = 0 ]; then
+   echo -e "${RED}              . . . ."
+   echo -e "${RED}              The script needs to be run as root. Rerun this script, with sudo. " >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
+
+# Commands to Run and Memorialize - Ports
 echo "Common TCP Port Scan Running. . ."
-sleep 2m;
-nmap -Pn -p- $ip > $ip_fulltcp_nmap.txt;
-echo "Full TCP Port Scan Running. . ."
-sleep 2m;
-nmap -Pn -sU $ip > $ip_commonUDP_nmap.txt
-echo "Common UDP Port Scan Running. . ."
+nmap -Pn $1 > $1'_commonTCP_nmap.txt'
 
+echo "Full TCP Port Scan Running. . . This may take a while, grab a coffee."
+nmap -Pn -p- -sV -O $1 > $1'_fullTCP_nmap.txt'
+
+#echo "Common UDP Port Scan Running. . . Hopefully this doesnt take as long as the last scan."
+#sudo nmap -Pn -sU $1> $1'_commonUDP_nmap.txt'
+
+
+# Commands to Run and Memorialize - Web Services
+if cat $1_commonTCP_nmap.txt |
+	grep -qFe "80/tcp   open"
+then
+   echo "Running Nikto Scan on http (Port 80) service. . ."
+   nikto -h http://$1/ > $1_http_nikto.txt
+elif cat $1_commonTCP_nmap.txt | 
+	grep -qFe "443/tcp   open"
+   echo "Running Nikto Scan on https (Port 443) service. . ."
+   nikto -h http://$1/ > $1_http_nikto.txt
+else
+   echo "Web services not detected within the environment. Nikto is not running."
+fi 
+
+# Commands to zip directory
